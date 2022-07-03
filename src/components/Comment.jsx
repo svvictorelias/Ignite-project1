a119ac869cafb02dd339e107dfc9d1ec5fc3bcd4
Victor Elias <svvictorelias@gmail.com>
2022-07-03 16:27:26 -0300

    Entendendo Key React

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,7 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
-export function Comment() {
+export function Comment({content}) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/svvictorelias.png" />
@@ -18,7 +18,7 @@ export function Comment() {
               <Trash size={24} />
             </button>
           </header>
-          <p>Muito bom esse parabéns</p>
+          <p>{content}</p>
         </div>
         <footer>
           <button>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,18 +1,32 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState(['Post muito Legaal!']);
+  const [newCommentText, setNewCommentText] = useState('');
   const publishedDateFormatted = format(
     publishedAt,
-    "dd 'de' LLLL 'às' HH:mm'h'",{locale: ptBR}
+    "dd 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
   );
 
-  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true
-  })
+  });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, newCommentText]);
+    setNewCommentText('')
+  }
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
   return (
     <article className={styles.post}>
       <header>
@@ -24,31 +38,43 @@ export function Post({ author, publishedAt, content }) {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        {content.map(line=>{
-          if(line.type =='paragraph'){
-            return <p>{line.content}</p>
-          }else if(line.type=='link'){
-            return <p><a href="#">{line.content}</a></p>
+        {content.map(line => {
+          if (line.type == 'paragraph') {
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.type == 'link') {
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          onChange={handleNewCommentChange}
+          name="comment"
+          value={newCommentText}
+          placeholder="Deixe um comentário"
+        />
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment key={comment} content={comment} />;
+        })}
       </div>
     </article>
   );
